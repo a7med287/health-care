5707fd7c05788fc479eb19865afe5257c201b660
AIO 2025 <rezk7672@gmail.com>
2025-12-07 15:16:28 +0200

    save token in login success

--- lib/features/auth/login/presentaion/views/widgets/login_view_body_block_consumer.dart
+++ lib/features/auth/login/presentaion/views/widgets/login_view_body_block_consumer.dart
@@ -4,6 +4,7 @@ import 'package:health_care/features/home/presentation/views/home_view.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../../../../../../core/helpers/build_snak_bar.dart';
+import '../../../../../../core/storage/token_storage.dart';
 import '../../../../verify/presentation/views/verify_view.dart';
 import '../../login_cubit/login_cubit.dart';
 import 'login_view_body.dart';
@@ -12,30 +13,29 @@ class LoginViewBodyBlockConsumer extends StatelessWidget {
   const LoginViewBodyBlockConsumer({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
     return BlocConsumer<LoginCubit, LoginState>(
-      listener: (context, state) {
+      listener: (context, state) async{
         if (state is LoginSuccess) {
           buildSnackBar(context, "Login Successful");
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeView(),));
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomeView()),
+          );
+          final token = await TokenStorage().getToken();
+          print(" token is: $token");
         }
 
         if (state is LoginOtpRequired) {
           buildSnackBar(context, "Verify your email");
           Navigator.push(
             context,
-            MaterialPageRoute(
-              builder: (_) => VerifyView(email: state.email),
-            ),
+            MaterialPageRoute(builder: (_) => VerifyView(email: state.email)),
           );
         }
 
         if (state is LoginFailure) {
-          buildSnackBar(
-            context,
-            state.message,
-            isError: true,
-          );
+          buildSnackBar(context, state.message, isError: true);
         }
       },
       builder: (context, state) {
